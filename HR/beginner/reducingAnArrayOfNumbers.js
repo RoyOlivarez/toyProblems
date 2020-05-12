fdc5e35c1374de758eacdd8d214624e49b4298fa
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:25:21 -0500

    added file and psuedocode

---
+++ HR/beginner/reducingAnArrayOfNumbers.js
@@ -0,0 +1,16 @@
+/*
+  Basic Set 3.1: Reducing an array of numbers
+    Given an array of numbers, return the sum of those numbers.
+    DO NOT use numbers.reduce()
+    DO use a for loop.
+    TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+    Examples:
+      Input	
+        numbers: [ 10, 20, 30 ]	
+      
+      Output: 60
+      Input	
+        numbers: []	
+      
+      Output: 0
+*/
