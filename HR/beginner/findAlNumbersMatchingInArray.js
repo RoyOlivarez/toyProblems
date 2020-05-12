e93a35103ae2f8ec588624feb8af819244b35c05
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:15:34 -0500

    added psuedocode to file

--- HR/beginner/findAlNumbersMatchingInArray.js
+++ HR/beginner/findAlNumbersMatchingInArray.js
@@ -1,1 +1,12 @@
-'This is a test';
+/*
+Basic Set 2.2: Finding all matching numbers in an array
+  Given an array of numbers, return all even numbers from the array.
+  DO NOT use .filter() in your solution.
+  DO use a for loop.
+  TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+  Examples
+    Input	
+      numbers:[ 10, 20, 25, 30 ]
+    Output
+      [ 10, 20, 30 ]
+*/
