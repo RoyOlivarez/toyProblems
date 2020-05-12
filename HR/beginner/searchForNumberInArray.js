25cc38ea07a809c4a4cb1d078d8dad6d53778898
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:28:56 -0500

    added file and psuedocode

---
+++ HR/beginner/searchForNumberInArray.js
@@ -0,0 +1,20 @@
+/*
+  Basic Set 1.2: Searching for a number in an array
+    Given a divider number and an array of numbers, return true if the array contains a non-zero number evenly divisible by the divider, and false otherwise.
+    DO NOT use array.find()
+    DO use a for loop.
+    TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+    Examples
+    
+    Input
+      divider: 5
+      array: [ 8, 10 ]
+    Output
+      true
+    
+    Input
+      divider: 5
+      array: [ 8, 11 ]
+    Output
+      false
+*/
