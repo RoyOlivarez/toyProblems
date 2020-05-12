955170e7d9652bbddbad1cd9b326200f21821922
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:21:15 -0500

    added new file and psuedocode

---
+++ HR/beginner/findAllMatchingStringsInArray.js
@@ -0,0 +1,18 @@
+/*
+  Basic Set 2.1: Finding all matching strings in an array
+    Given a length (number) and an array of strings, return all strings that are greater than or equal to the given length.
+    DO NOT use .filter() in your solution.
+    DO use a for loop.
+    TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+    Examples
+    
+      Input
+        length:3
+        array: [ "a", "ab", "abc", "abcd" ]	
+      Output: [ "abc", "abcd" ]
+    
+      Input
+        length:2
+        array:[ "x", "hello", "z" ]
+      Output: ["hello"]
+*/
