8e6f6fd710a7e9232bfa493d00034f9f12123224
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:23:40 -0500

    added file and psuedocode

---
+++ HR/beginner/reducingAnArrayOfArrays.js
@@ -0,0 +1,21 @@
+/*
+  Basic Set 3.3: Reducing an array of arrays
+    Given an array of arrays, return those arrays concatenated together.
+  Examples
+    Input	
+      arrays: [ [ 10, 20 ], [ 30 ], [ ] ]	
+    
+    Output
+      [ 10, 20, 30 ]
+    
+    Input
+      arrays: [ [ 22 ], [ 33, [ 44 ] ], [ 55 ] ]	
+    
+    Output
+      [ 22, 33, [ 44 ], 55 ]
+    
+    Input
+      arrays:[ ]
+    Output
+      []
+*/
