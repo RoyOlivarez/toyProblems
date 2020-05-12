1f5a314f039ec14cd1fd300414f54fe389da6345
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:29:52 -0500

    added file and psuedocode

---
+++ HR/beginner/searchForObjectInArray.js
@@ -0,0 +1,15 @@
+/*
+  Basic Set 1.3: Searching for an object in an array
+    Given a name (string) and an array of people (objects), 
+    find the person with the given name and return his/her hobby. 
+    Return false if there is no person with the given name.
+  DO NOT use people.find()
+  DO use a for loop.
+  TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+Examples
+  Input	
+    name: "Alice"
+    people: [ { "name": "Alice", "hobby": "programming" }, { "name": "Bob", "hobby": "gaming" } ]
+  Output:
+    'programming'
+*/
