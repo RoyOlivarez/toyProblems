301efc49bd7fba83148ee227772dab7756e5de8e
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:27:25 -0500

    added file and psuedocode

---
+++ HR/beginner/reducingAnArrayOfStrings.js
@@ -0,0 +1,18 @@
+/*
+  Basic Set 3.2: Reducing an array of strings
+    Given an array of strings, return all strings concatenated together.
+    DO NOT use strings.reduce() or strings.join('')
+    DO use a for loop.
+    TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+    Examples
+      Input	
+        strings:[ "a", "b", "c" ]	
+      
+      Output
+        "abc"
+      
+      Input	
+        strings:  [ ]
+      Output
+        ""
+*/
