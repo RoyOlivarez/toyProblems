ee1296bb4f490e4768aa5870159bddcf76a304c8
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:32:03 -0500

    added file and psuedocode

---
+++ HR/beginner/searchForStringInArray.js
@@ -0,0 +1,7 @@
+/*
+Basic Set 1.1: Searching for a string in an array
+  Given a string and an array of strings, return true if the string exists in the array, and false if it does not.
+  DO NOT use .includes() in your solution.
+  DO use a for loop.
+  TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+*/
