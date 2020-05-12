ecf517cd42e0fb7fe98d45c6d24b9c7f40ae1a28
RoyOlivarez <royolivarez66@gmail.com>
2020-05-12 18:17:29 -0500

    added new file and psuedocode

---
+++ HR/beginner/findAllMatchingNumbersInArray.js
@@ -0,0 +1,12 @@
+/*
+  Basic Set 2.3: Finding all matching objects in an array
+    Given an array of players (objects), return the names of all players that have a score above 100.
+    DO NOT use .filter() in your solution.
+    DO use a for loop.
+    TRY solving it with non-mutative recursion, only after you’ve solved it using a for loop.
+    Examples
+      Input
+        players: [ { "name": "Alice", "score": 88 }, { "name": "Bob", "score": 120 } ]
+    	Output
+        [ "Bob" ]
+*/
