5a2e46575abc894cb2ea2637834c24c41656061a
RoyOlivarez <royolivarez66@gmail.com>
2020-10-14 13:38:37 -0500

    working on updated solution

--- HR/46/bubbleSort/bubbleSort.js
+++ HR/46/bubbleSort/bubbleSort.js
@@ -29,3 +29,12 @@
  */
 
 // Feel free to add helper function if needed
+
+const fizzBuzz = () => {
+  for (let i = 1; i <= 50; i++) {
+    if (i % 15 === 0) console.log('fizzBuzz');
+    else if (i % 3 === 0) console.log('Fizz'); 
+    else if (i % 5 === 0) console.log('Buzz');
+    else console.log(i);
+  };
+};
